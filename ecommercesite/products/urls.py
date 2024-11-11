705a7e04dc16aac1ba80392ef9e856a1d3ae6367
12siragi <siragisaidi218@gmail.com>
2024-11-11 14:40:09 +0300

    store

---
+++ ecommercesite/products/urls.py
@@ -0,0 +1,11 @@
+# products/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'products'  # This should be at the top
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('product/<int:id>/<slug:slug>/', views.product_detail, name="product_detail"),
+]
